789b4944005844ae8a6979366d46467a1b8d3e44
Jiarui-Xu-Gatech <77424292+Jiarui-Xu-Gatech@users.noreply.github.com>
2022-03-13 16:59:05 -0400

    Update Test_Vibrato.cpp

--- src/Tests/Tests/Test_Vibrato.cpp
+++ src/Tests/Tests/Test_Vibrato.cpp
@@ -18,8 +18,45 @@ namespace vibrato_test {
         }
     }
 
-    void RingBufferTest(int iRingBuffLength) {
+    class RingBuffer : public testing::Test
+    {
+    protected:
+        void SetUp() override
+        {
+            pCRingBuffer = new CRingBuffer<float>(kBlockSize);
+        }
+
+        virtual void TearDown()
+        {
+            delete[] pCRingBuffer;
+            pCRingBuffer = 0;
+        }
+
         CRingBuffer<float>* pCRingBuffer = 0;
+        static const int kBlockSize = 17;
+    };
+
+    class Vibrato : public testing::Test
+    {
+    public:
+        void SetUp() override
+        {
+            CVibratoIf::create(pCVibrato);
+
+        }
+
+        virtual void TearDown()
+        {
+            CVibratoIf::destroy(pCVibrato);
+        }
+
+        float* ppfInputBuffer[2];
+        float* ppfOutputBuffer[2];
+        CVibratoIf* pCVibrato;
+    };
+
+    TEST_F(RingBuffer, RingBufferTest) {
+        int iRingBuffLength = pCRingBuffer->getLength();
         pCRingBuffer = new CRingBuffer<float>(iRingBuffLength);
         for (int i = 0; i < 5; i++) {
             pCRingBuffer->putPostInc(1.F * i);
@@ -56,7 +93,7 @@ namespace vibrato_test {
         pCRingBuffer->reset();
         pCRingBuffer->putPostInc(1.F);
         for (int i = 0; i < 20; i++) {
-            std::cout<<"empty_test"<< pCRingBuffer->getPostInc() << std::endl;
+            std::cout << "empty_test" << pCRingBuffer->getPostInc() << std::endl;
         }
 
         //test5 read before write
@@ -64,12 +101,12 @@ namespace vibrato_test {
         pCRingBuffer->reset();
         for (int i = 0; i < 5; i++)
         {
-            std::cout <<"read before write"<< pCRingBuffer->getPostInc() << std::endl;
+            std::cout << "read before write" << pCRingBuffer->getPostInc() << std::endl;
         }
 
         for (int i = 1; i < 25; i++)
         {
-            std::cout <<"for " << i << std::endl;
+            std::cout << "for " << i << std::endl;
             std::cout << pCRingBuffer->getNumValuesInBuffer() << " "; // is 5
             std::cout << pCRingBuffer->getPostInc() << std::endl; // is i-5
             pCRingBuffer->putPostInc(1.F * i);
@@ -77,9 +114,9 @@ namespace vibrato_test {
 
     }
 
-    void ZeroAmplitudeTest(float** ppfInputBuffer,float** ppfOutputBuffer,CVibratoIf* pCVibrato) {
+    TEST_F(Vibrato, ZeroAmplitudeTest) {
         std::cout << "ZeroAmplitude Test" << std::endl;
-        pCVibrato->init(0.1, 5, 1, 44100, 0.01);
+        pCVibrato->init(1, 16000, 0.01,0.1);
         ppfInputBuffer[0] = new float[1024];
         ppfOutputBuffer[0] = new float[1024];
         for (int i = 0; i < 1024; i++)
@@ -95,12 +132,12 @@ namespace vibrato_test {
         std::cout << "ZeroAmplitudeTest Pass!" << std::endl;
     }
 
-    void DCInputDCOutputTest(float** ppfInputBuffer, float** ppfOutputBuffer, CVibratoIf* pCVibrato) {
+    TEST_F(Vibrato, DCInputDCOutputTest) {
         std::cout << "DCInputDCOutput Test" << std::endl;
-        
-        pCVibrato->init(0.1, 5, 1, 44100, 0.01);
-//        pCVibrato->setParam(CVibratoIf::kParamFreqMod, 10);
-//        pCVibrato->setParam(CVibratoIf::kParamDelay, 0.005);
+
+        pCVibrato->init(1, 16000, 0.01, 0.01);
+        pCVibrato->setParam(CVibratoIf::kParamFreqMod, 10);
+        pCVibrato->setParam(CVibratoIf::kParamDelay, 0.005);
         ppfInputBuffer[0] = new float[1024];
         ppfOutputBuffer[0] = new float[1024];
         for (int i = 0; i < 1024; i++)
@@ -108,12 +145,12 @@ namespace vibrato_test {
             ppfInputBuffer[0][i] = 1;
         }
         pCVibrato->process(ppfInputBuffer, ppfOutputBuffer, 1024);
-        CHECK_ARRAY_CLOSE(ppfInputBuffer[0] + 80, ppfOutputBuffer[0] + 160+80, 1024 - 160-80, 1e-4);
+        CHECK_ARRAY_CLOSE(ppfInputBuffer[0] + 80, ppfOutputBuffer[0] + 160 + 80, 1024 - 160 - 80, 1e-4);
 
-//        pCVibrato->setParam(CVibratoIf::kParamFreqMod, 20);
-//        pCVibrato->setParam(CVibratoIf::kParamDelay, 0.003);
+        pCVibrato->setParam(CVibratoIf::kParamFreqMod, 20);
+        pCVibrato->setParam(CVibratoIf::kParamDelay, 0.003);
         pCVibrato->process(ppfInputBuffer, ppfOutputBuffer, 1024);
-        CHECK_ARRAY_CLOSE(ppfInputBuffer[0] + 48, ppfOutputBuffer[0] + 160+48, 1024 - 160-48, 1e-4);
+        CHECK_ARRAY_CLOSE(ppfInputBuffer[0] + 48, ppfOutputBuffer[0] + 160 + 48, 1024 - 160 - 48, 1e-4);
 
 
         delete[] ppfInputBuffer[0];
@@ -123,11 +160,11 @@ namespace vibrato_test {
         std::cout << "DCInputDCOutput Pass!" << std::endl;
     }
 
-    void VaryBlockSizeTest(float** ppfInputBuffer, float** ppfOutputBuffer, CVibratoIf* pCVibrato) {
+    TEST_F(Vibrato, VaryBlockSizeTest) {
         std::cout << "VaryBlockSize Test" << std::endl;
-        pCVibrato->init(0.1, 5, 1, 44100, 0.01);
-//        pCVibrato->setParam(CVibratoIf::kParamFreqMod, 10);
-//        pCVibrato->setParam(CVibratoIf::kParamDelay, 0.005);
+        pCVibrato->init(1, 16000, 0.01, 0.1);
+        pCVibrato->setParam(CVibratoIf::kParamFreqMod, 10);
+        pCVibrato->setParam(CVibratoIf::kParamDelay, 0.005);
         ppfInputBuffer[0] = new float[1024];
         ppfOutputBuffer[0] = new float[1024];
         for (int i = 0; i < 1024; i++)
@@ -141,10 +178,10 @@ namespace vibrato_test {
             float* pfOutputBuffer_i = ppfOutputBuffer[0] + i;
             pCVibrato->process(&pfInputBuffer_i, &pfOutputBuffer_i, i);
         }
-        CHECK_ARRAY_CLOSE(ppfInputBuffer[0] + 80, ppfOutputBuffer[0] + 160+80, 1024 - 160-80, 1e-4);
+        CHECK_ARRAY_CLOSE(ppfInputBuffer[0] + 80, ppfOutputBuffer[0] + 160 + 80, 1024 - 160 - 80, 1e-4);
 
-//        pCVibrato->setParam(CVibratoIf::kParamFreqMod, 20);
-//        pCVibrato->setParam(CVibratoIf::kParamDelay, 0.003);
+        pCVibrato->setParam(CVibratoIf::kParamFreqMod, 20);
+        pCVibrato->setParam(CVibratoIf::kParamDelay, 0.003);
         pCVibrato->process(ppfInputBuffer, ppfOutputBuffer, 32);
         for (int i = 32; i < 1024; i <<= 1)
         {
@@ -161,11 +198,11 @@ namespace vibrato_test {
         std::cout << "VaryBlockSize Pass!" << std::endl;
     }
 
-    void ZeroInputTest(float** ppfInputBuffer, float** ppfOutputBuffer, CVibratoIf* pCVibrato) {
+    TEST_F(Vibrato, ZeroInputTest) {
         std::cout << "ZeroInput Test" << std::endl;
-        pCVibrato->init(0.1, 5, 1, 44100, 0.01);
-//        pCVibrato->setParam(CVibratoIf::kParamFreqMod, 10);
-//        pCVibrato->setParam(CVibratoIf::kParamDelay, 0.005);
+        pCVibrato->init(1, 16000, 0.01, 0.01);
+        pCVibrato->setParam(CVibratoIf::kParamFreqMod, 10);
+        pCVibrato->setParam(CVibratoIf::kParamDelay, 0.005);
         ppfInputBuffer[0] = new float[1024];
         ppfOutputBuffer[0] = new float[1024];
         for (int i = 0; i < 1024; i++)
@@ -173,16 +210,16 @@ namespace vibrato_test {
             ppfInputBuffer[0][i] = 0;
         }
         pCVibrato->process(ppfInputBuffer, ppfOutputBuffer, 1024);
-        CHECK_ARRAY_CLOSE(ppfInputBuffer[0] + 80, ppfOutputBuffer[0] + 160+80, 1024 - 160-80, 1e-4);
+        CHECK_ARRAY_CLOSE(ppfInputBuffer[0] + 80, ppfOutputBuffer[0] + 160 + 80, 1024 - 160 - 80, 1e-4);
 
-//        pCVibrato->setParam(CVibratoIf::kParamFreqMod, 20);
-//        pCVibrato->setParam(CVibratoIf::kParamDelay, 0.003);
+        pCVibrato->setParam(CVibratoIf::kParamFreqMod, 20);
+        pCVibrato->setParam(CVibratoIf::kParamDelay, 0.003);
         pCVibrato->process(ppfInputBuffer, ppfOutputBuffer, 1024);
         for (int i = 0; i < 1024; i++)
         {
             ppfInputBuffer[0][i] = 0;
         }
-        CHECK_ARRAY_CLOSE(ppfInputBuffer[0] + 48, ppfOutputBuffer[0] + 160+48, 1024 - 160-48, 1e-4);
+        CHECK_ARRAY_CLOSE(ppfInputBuffer[0] + 48, ppfOutputBuffer[0] + 160 + 48, 1024 - 160 - 48, 1e-4);
 
         delete[] ppfInputBuffer[0];
         delete[] ppfOutputBuffer[0];
@@ -192,10 +229,10 @@ namespace vibrato_test {
     }
 
     //add test:set parameters
-    void SetParaTest(float** ppfInputBuffer, float** ppfOutputBuffer, CVibratoIf* pCVibrato) {
+    TEST_F(Vibrato, SetParaTest) {
         std::cout << "SetParameters Test" << std::endl;
-//        EXPECT_EQ(pCVibrato->setParam(CVibratoIf::kParamFreqMod, -1.5f), Error_t::kFunctionInvalidArgsError);
-//        EXPECT_EQ(pCVibrato->setParam(CVibratoIf::kParamDelay, -0.1f), Error_t::kFunctionInvalidArgsError);
+        EXPECT_EQ(pCVibrato->setParam(CVibratoIf::kParamFreqMod, -1.5f), Error_t::kFunctionInvalidArgsError);
+        EXPECT_EQ(pCVibrato->setParam(CVibratoIf::kParamDelay, -0.1f), Error_t::kFunctionInvalidArgsError);
         std::cout << "SetParameters Pass!" << std::endl;
     }
 
